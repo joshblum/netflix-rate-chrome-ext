e6d6081ba4e7962d0cc752d0f633331678ddfdb7
Joshua Blum <joshblum@mit.edu>
2013-01-30 20:34:16 -0500

    popup change and side title parsing

---
+++ js/popup.js
@@ -0,0 +1,1 @@
+chrome.tabs.create({url: document.getElementById('netflix').href});
--- js/ratings.js
+++ js/ratings.js
@@ -7,54 +7,55 @@
 // callback	 name (optional)	 JSONP callback name
 // tomatoes	 true (optional)	 adds rotten tomatoes data
 var IMDB_API =  "http://www.omdbapi.com/?tomatoes=true&t=";
-var HOVER_SEL = '.bobbable .popLink';
+var HOVER_SEL = {
+		'.bobbable .popLink' : getMainTitle, //main display movies
+		'.mdpLink' : getSideTitle, //small side movies
+	};
 var POPUP_INS_SEL = ".midBob";
 var POPUP_SEL = ".bobMovieRatings";
 var CACHE = {};
-var currentRating = {
-	'imdb' : null,
-	'tomato' : null,
+
+/*
+	Add the style sheet to the main netflix page.
+*/
+function addStyle() {
+	if (!$('#rating-overlay').length){
+		var url = chrome.extension.getURL('../css/ratings.css');
+		$("head").append("<link id='rating-overlay' href='" + url + "' type='text/css' rel='stylesheet' />");
+	}
 }
 
 function getIMDBAPI(title) {
 	return IMDB_API + title
 }
 
-function getIMDBHtml(score) {
-	var html = $('<div class="imdb imdb-icon star-box-giga-star">');
-	if (score === null) {
-		html.css('visibility', 'hidden');
-	} else {
-		html.append(score);
-	}
-	return html
-}
-
-function getTomatoHtml(score) {
-	var html = $('<span class="tomato tomato-wrapper">' +
-		    	'<span class="tomato-icon med"></span>' +
-		        '<span class="tomato-score"></span>' +
-        	'<span>');
-	if (score === null) {
-		html.css('visibility', 'hidden');
-		return html
-	}
-	var klass;
-	if (score < 59) {
-		klass = 'rotten';
-	} else {
-		klass = 'fresh';
-	}
-	html.find('.tomato-icon').addClass(klass);
-	html.find('.tomato-score').append(score + '%');
-	return html
-}
-
-function getTitle(e) {
+/*
+	parses form: http://movies.netflix.com/WiPlayer?movieid=70171942&trkid=7103274&t=Archer
+*/
+function getMainTitle(e) {
 	var url = $(e.target).context.href;
 	var title = url.split('&t=')[1];
 	title = decodeURIComponent(title).replace(/\+/g, ' ');
 	return title
+}
+
+function getSideTitle(e) {
+	var title = $(e.target).attr('alt');
+	if (title === undefined) {
+		var url = $(e.target).context.href;
+		url = url.split('/')
+		var title = url[url.indexOf('WiMovie') + 1]
+		title = title.replace(/_/g, ' ')
+	}
+	return title
+}
+
+function eventHandler(e){
+	var url = $(e.target).context.href;
+	var title = e.data(e) //title parse funtion
+	getRating(title, function(rating){
+		showRating(rating);
+	});
 }
 
 function addCache(title, imdb, tomato) {
@@ -92,29 +93,50 @@ function showRating(rating) {
 		var $target = $(POPUP_INS_SEL);
 		if($target.length){	
 		    clearInterval(checkVisible);
-		    	$('.tomato').remove();
+		    $('.tomato').remove();
 			$('.imdb').remove();
 			$('.ratingPredictor').remove();
+			$('.label').contents().remove()
 			$target.append(imdb);
 			$target.append(tomato);
 		}
-	}, 700);
+	}, 650);
 }
 
-function addStyle() {
-	if (!$('#rating-overlay').length){
-		var url = chrome.extension.getURL('../css/ratings.css');
-		$("head").append("<link id='rating-overlay' href='" + url + "' type='text/css' rel='stylesheet' />");
+function getIMDBHtml(score) {
+	var html = $('<div class="imdb imdb-icon star-box-giga-star">');
+	if (score === null) {
+		html.css('visibility', 'hidden');
+	} else {
+		html.append(score);
 	}
+	return html
+}
+
+function getTomatoHtml(score) {
+	var html = $('<span class="tomato tomato-wrapper">' +
+		    	'<span class="tomato-icon med"></span>' +
+		        '<span class="tomato-score"></span>' +
+        	'<span>');
+	if (score === null) {
+		html.css('visibility', 'hidden');
+		return html
+	}
+	var klass;
+	if (score < 59) {
+		klass = 'rotten';
+	} else {
+		klass = 'fresh';
+	}
+	html.find('.tomato-icon').addClass(klass);
+	html.find('.tomato-score').append(score + '%');
+	return html
 }
 
 $(document).ready(function() {
 	addStyle();
+	$.each(HOVER_SEL, function(selector, parser){
 
-	$(document).on('mouseenter', HOVER_SEL, function(e){
-		var title = getTitle(e)
-		getRating(title, function(rating){
-			showRating(rating);
-		});
+		$(document).on('mouseenter', selector, parser, eventHandler);
 	});
 });
