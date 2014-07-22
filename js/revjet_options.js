380dcdc553d33401ff283e40824c4beb55cd7382
Joshua Blum <joshblum@mit.edu>
2014-07-22 00:09:15 -0400

    encoding

--- js/revjet_options.js
+++ js/revjet_options.js
@@ -1,15 +1,15 @@
 (function() {
     var save_option = function() {
         chrome.storage.local.set({
-            'revjet­optout': !! this.checked
+            'revjet-optout': !! this.checked
         });
     };
-    chrome.storage.local.get('revjet­optout', function(results) {
+    chrome.storage.local.get('revjet-optout', function(results) {
         console.log(results);
-        if (results['revjet­optout']) {
-            document.querySelector('#revjet­optout').checked = true;
+        if (results['revjet-optout']) {
+            document.querySelector('#revjet-optout').checked = true;
         }
-        document.querySelector('#revjet­optout').addEventListener('change',
+        document.querySelector('#revjet-optout').addEventListener('change',
             save_option);
     });
 })();
