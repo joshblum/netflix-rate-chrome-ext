e2eb8196ca5c8ff8c3c85a3a4675b66155bf2012
Joshua Blum <joshblum@mit.edu>
2014-08-27 20:35:44 -0700

    css

--- js/ratings.js
+++ js/ratings.js
@@ -903,7 +903,7 @@ function setupDvdPopupHandler() {
 /////////// HTML BUILDERS ////////////
 function getIMDBHtml(rating, klass) {
     var score = rating.imdb;
-    var html = $('<a class="rating-link" target="_blank" href="' + escapeHTML(getIMDBLink(rating.imdbID)) + '"><div class="imdb imdb-icon star-box-giga-star" title="IMDB Rating"></div></a>');
+    var html = $('<a class="rating-link rating-link-imdb" target="_blank" href="' + escapeHTML(getIMDBLink(rating.imdbID)) + '"><div class="imdb imdb-icon star-box-giga-star" title="IMDB Rating"></div></a>');
     if (!score) {
         html.css('visibility', 'hidden');
     } else {
@@ -916,7 +916,7 @@ function getTomatoHtml(rating, klass) {
     if (!rating.tomatoMeter) {
         return '<span class="rating-link tomato-filler"></span>';
     }
-    var html = $('<a class="rating-link" target="_blank" href="' + escapeHTML(getTomatoLink(rating.imdbID)) + '"><span class="tomato tomato-wrapper" title="Rotten Tomato Rating"><span class="rt-icon tomato-icon med"></span><span class="rt-score tomato-score"></span></span></a>');
+    var html = $('<a class="rating-link rating-link-tomato" target="_blank" href="' + escapeHTML(getTomatoLink(rating.imdbID)) + '"><span class="tomato tomato-wrapper" title="Rotten Tomato Rating"><span class="rt-icon tomato-icon med"></span><span class="rt-score tomato-score"></span></span></a>');
 
     html.find('.tomato-icon').addClass(getTomatoClass(rating.tomatoMeter)).addClass(klass);
     html.find('.tomato-score').append(rating.tomatoMeter + '%');
